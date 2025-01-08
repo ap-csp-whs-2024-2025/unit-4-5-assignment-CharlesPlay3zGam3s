e45f8e9d2084ef0e8373d9dd4713ad1c266fb5e4
Mr. Charles <94595457+CharlesPlay3zGam3s@users.noreply.github.com>
2025-01-08 21:03:44 +0000

    learn day back

--- classwork.cpp
+++ classwork.cpp
@@ -8,7 +8,15 @@ void displayList(std::vector<T> const &v);
 int main()
 {
     // You can follow along with any code in class here
+    int x = 5;
+    double t = 3.14;
+    std:: string name - "Mr. Vu";
 
+    // A list of ints named jennys with 7 elemnets in it
+    std::vectpr<int> jennys = {8,6,7,5,3,0,9};
+    std::vector<double> foo = {3.14,2.5};
+    std::vector<std::string> bar = {"hello", "darkness", "my", "old", "friend"}
+    std::vects
     
     return 0;
 }
